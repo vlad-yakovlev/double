d5a4578d90d437b232513898386eab04e8f9b358
Vladislav Yakovlev <vlad309523@gmail.com>
2022-05-23 03:35:51 +0400

    Fix deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_BASE_PATH || '';
+
 const nextConfig = {
+  assetPrefix: `${basePath}/`,
+  basePath,
   reactStrictMode: true,
 };
 
