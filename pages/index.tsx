da0eac9a4eccb8163383a558e604bfca2eb78160
Vladislav Yakovlev <vlad309523@gmail.com>
2022-05-23 03:24:52 +0400

    Add docker

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { Game } from '../components/Game';
 
 const Home: NextPage = () => (
   <div className="flex items-center justify-center min-h-screen">
+    <Head>
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+      />
+
+      <title>Double</title>
+    </Head>
+
     <Game />
   </div>
 );
